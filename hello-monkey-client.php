0204ddd3d3003951a1580e31bf116a80d46db747
ste_dean <ste_dean@hotmail.com>
2015-01-12 07:06:21 +0000

    update

---
+++ hello-monkey-client.php
@@ -0,0 +1,52 @@
+<?php
+include 'twilio-php-master/Services/Twilio/Capability.php';
+ 
+// put your Twilio API credentials here
+$accountSid = 'ACfa638e607f8eb06cc2aa87e67c96a364';
+$authToken  = 'a0e5d44bc1c7875f1aedabbc5be4c88c';
+ 
+$capability = new Services_Twilio_Capability($accountSid, $authToken);
+$capability->allowClientOutgoing('APabe7650f654fc34655fc81ae71caa3ff');
+$token = $capability->generateToken();
+?>
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello Client Monkey 1</title>
+    <script type="text/javascript"
+      src="//static.twilio.com/libs/twiliojs/1.2/twilio.min.js"></script>
+    <script type="text/javascript"
+      src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js">
+    </script>
+    <link href="http://static0.twilio.com/packages/quickstart/client.css"
+      type="text/css" rel="stylesheet" />
+    <script type="text/javascript">
+ 
+      Twilio.Device.setup("<?php echo $token; ?>");
+ 
+      Twilio.Device.ready(function (device) {
+        $("#log").text("Ready");
+      });
+ 
+      Twilio.Device.error(function (error) {
+        $("#log").text("Error: " + error.message);
+      });
+ 
+      Twilio.Device.connect(function (conn) {
+        $("#log").text("Successfully established call");
+      });
+ 
+      function call() {
+        Twilio.Device.connect();
+      }
+    </script>
+  </head>
+  <body>
+    <button class="call" onclick="call();">
+      Call
+    </button>
+ 
+    <div id="log">Loading pigeons...</div>
+  </body>
+</html>
