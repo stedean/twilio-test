815942d0fa95a3d474c46fc278200a6b608d35f0
ste_dean <ste_dean@hotmail.com>
2015-01-14 17:29:30 +0000

    update

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+ $accountsid = 'ACfa638e607f8eb06cc2aa87e67c96a364'; // YOUR TWILIO ACCOUNT SID
+ $authtoken = 'a0e5d44bc1c7875f1aedabbc5be4c88c'; // YOUR TWILIO AUTH TOKEN
+ $fromNumber = '+447788310101'; // PHONE NUMBER CALLS WILL COME FROM
+ $toNumber = '+447788310101'; // YOUR PHONE NUMBER TO CONNECT TO
+?>
