e5e238a6e5c2f2e14afb81b147914a4ef76d43c7
ste_dean <ste_dean@hotmail.com>
2015-01-14 17:38:28 +0000

    update

--- makecall.php
+++ makecall.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
+echo("Including twilio");
 include 'twilio-php-master/Services/Twilio.php';
+echo("<br>including config");
 include("config.php");
+echo("<br>creating client");
 $client = new Services_Twilio($accountsid, $authtoken);
 if (!isset($_REQUEST['called'])) {
+ echo("<br>No phone number");
  $err = urlencode("Must specify your phone number");
  header("Location: click-to-call.php?msg=$err");
  die;
 }
-$call = $client->account->calls->create($fromNumber,
- $toNumber,
- 'callback.php?number=' . $_REQUEST['called']);
+echo("<br>creating call");
+$call = $client->account->calls->create($fromNumber,$toNumber,'callback.php?number=' . $_REQUEST['called']);
 $msg = urlencode("Connecting... ".$call->sid);
 header("Location: click-to-call.php?msg=$msg");
 ?>
