d0151a9d662d4259c6fcd37c9d8078840ba09fc6
ste_dean <ste_dean@hotmail.com>
2015-01-22 15:59:51 +0000

    update

--- hello-client-twiml.php
+++ hello-client-twiml.php
@@ -2,7 +2,7 @@
 header('Content-type: text/xml');
  
 // put a phone number you've verified with Twilio to use as a caller ID number
-$callerId = "+441708394056";
+$callerId = "+447788310101";
  
 // put your default Twilio Client name here, for when a phone number isn't given
 $number   = "OpenVBX";
