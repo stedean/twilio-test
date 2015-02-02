a22e310c008ebe822efaacad2075f28e2d7606b6
ste_dean <ste_dean@hotmail.com>
2015-02-02 22:24:14 +0000

    update

--- client.php
+++ client.php
@@ -82,7 +82,10 @@ $token = $capability->generateToken();
 						else
 							connection.sendDigits(value)
 						return false;
-					} 
+					} else {
+					  $("#log").text("Pressed: " + value);
+					}
+					
 					});
 			});
 			
@@ -103,27 +106,27 @@ $token = $capability->generateToken();
  
     <div id="log">Loading...</div>
 
-    <div style="position: absolute; left: 50%;">
+    <div id="dialpad">
 				<table>
 				<tr>
-				<td><input id="dialpad" type="button" value="1" id="button1"></td>
-				<td><input  id="dialpad" type="button" value="2" id="button2"></td>
-				<td><input id="dialpad" type="button" value="3" id="button3"></td>
+				<td><input type="button" value="1" id="button1"></td>
+				<td><input type="button" value="2" id="button2"></td>
+				<td><input type="button" value="3" id="button3"></td>
 				</tr>
 				<tr>
-				<td><input id="dialpad" type="button" value="4" id="button4"></td>
-				<td><input id="dialpad" type="button" value="5" id="button5"></td>
-				<td><input id="dialpad" type="button" value="6" id="button6"></td>
+				<td><input type="button" value="4" id="button4"></td>
+				<td><input type="button" value="5" id="button5"></td>
+				<td><input type="button" value="6" id="button6"></td>
 				</tr>
 				<tr>
-				<td><input id="dialpad" type="button" value="7" id="button7"></td>
-				<td><input id="dialpad" type="button" value="8" id="button8"></td>
-				<td><input id="dialpad" type="button" value="9" id="button9"></td>
+				<td><input type="button" value="7" id="button7"></td>
+				<td><input type="button" value="8" id="button8"></td>
+				<td><input type="button" value="9" id="button9"></td>
 				</tr>
 				<tr>
-				<td><input id="dialpad" type="button" value="*" id="buttonstar"></td>
-				<td><input id="dialpad" type="button" value="0" id="button0"></td>
-				<td><input id="dialpad" type="button" value="#" id="buttonpound"></td>
+				<td><input type="button" value="*" id="buttonstar"></td>
+				<td><input type="button" value="0" id="button0"></td>
+				<td><input type="button" value="#" id="buttonpound"></td>
 				</tr>
 				</table>
 			</div>
